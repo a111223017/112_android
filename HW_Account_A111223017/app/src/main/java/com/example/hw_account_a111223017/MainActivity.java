ef8517bfae8c86e89f723fcd7f179d1262f953b7
a111223017 <a111223017@mail.shu.edu.tw>
2024-03-15 14:58:20 +0800

    113-03-15

---
+++ HW_Account_A111223017/app/src/main/java/com/example/hw_account_a111223017/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.hw_account_a111223017;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public  void  Button_click(View view)
+    {
+        EditText edtAccount = (EditText)findViewById(R.id.edtaccount);//建立關聯
+        EditText edtpassword = (EditText)findViewById(R.id.edtpassword);//建立關聯
+        TextView txvShow = (TextView)findViewById(R.id.txvShow);
+        String txtaccount = edtAccount.getText().toString();
+        String txtpassword  =(edtpassword.getText().toString());
+        if(txtaccount=="1" && txtpassword=="1")
+        {
+
+            txvShow.setText("登入成功");
+        }
+        else
+        {
+            txvShow.setText("登入..."+txtaccount+txtpassword);
+        }
+
+
+    }
+}
---
+++ HW_Account_A111223017/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "HW_Account_A111223017"
+include(":app")
+ 
