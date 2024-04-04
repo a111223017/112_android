a64ee7a8e9916ff8dc190d0f6eb4aff638e409c4
a111223017 <a111223017@mail.shu.edu.tw>
2024-04-04 17:06:35 +0800

    113-03-29-hw

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,19 +8,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 //bmi沒辦法除0x0 的另外狀況
 //改計算機的style 按鈕 加分 可以額外處理小數的運算 浮點數的運算
-enum State {FirstNumberInput, OperatorInputed, NumberInput}
-enum OP { None, Add, Sub, Mul, Div}
+
+enum State {FirstNumberInput, OperatorInputed, NumberInput, DecimalInput}
+enum OP { None, Add, Sub, Mul, Div,Dec}
 
 public class MainActivity extends AppCompatActivity {
-
     private int theValue = 0;
-    private int operand1=0, operand2=0;
+    private double thedValue = 0;
+    private String tempDecimalInput = "",tempinput="",tempinput2="";
+    private  double thedobvalue = 0;
+    private int operand1 = 0, operand2 = 0;
+    private double doperand1 = 0, doperand2 = 0;
     private OP op=OP.None;
     private State state = State.FirstNumberInput;
+
+    private  int doublenum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +51,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void processKeyInput(View view){
+        tempDecimalInput = "";
+
         Button b= (Button )view;    // 取得發生事件的按鈕
         String bstr= b.getText().toString();   // 取得發生事件的按鈕上的文字
         int bint; // 透過R.id.display取得顯示結果的EditText元件
         EditText edt = (EditText) findViewById(R.id.display);
 
+
         switch(bstr) { // 依據發生事件的按鈕上的文字值，進行不同的處理
             // 數字按鈕被點按時
+            case ".":
+
+                tempinput+=".";
+                edt.setText("" + tempinput);
+                break;
             case "0":
             case "1":
             case "2":
@@ -61,11 +76,14 @@ public class MainActivity extends AppCompatActivity {
             case "7":
             case "8":
             case "9":
-                bint = (new Integer(bstr)).intValue(); //將文字轉換為整數值
+
+                bint = Integer.parseInt(bstr);
+                double bdouble = (double)bint;
+
 
                 switch(state) { // 依據當時的狀態決定不同的處理
                     case FirstNumberInput:
-                        theValue=theValue*10+bint;
+                        tempinput+=bstr;
                         break;
                     case OperatorInputed:
                         theValue=bint;
@@ -73,12 +91,21 @@ public class MainActivity extends AppCompatActivity {
                         state=State.NumberInput;
                         break;
                     case NumberInput:
-                        theValue=theValue*10+bint;
+                        tempinput+=bstr;
+
                         break;
+                    case  DecimalInput:
+                        tempinput+=bstr;
+                        break;
+
+
                 }
-                edt.setText("" + theValue);
+                edt.setText("" + tempinput);
                 break;
+
+
             case "Clear": // 清除並重設相關變數
+                tempinput="";
                 state=State.FirstNumberInput;
                 theValue=0;
                 edt.setText((CharSequence)("0"));
@@ -86,77 +113,86 @@ public class MainActivity extends AppCompatActivity {
                 operand2=operand1=0;
                 break;
             case "Back": // 倒退鍵
+                StringBuilder bcaknumber = new StringBuilder(tempinput);
+                bcaknumber.deleteCharAt(bcaknumber.length() - 1);
+                String result = bcaknumber.toString();
+                tempinput =result;
                 theValue=(int)(theValue/10);
-                edt.setText("" + theValue);
+                edt.setText("" + tempinput);
                 break;
             case "+":
             case "-":
             case "*":
             case "/": // 當operator被點選時
-                switch(state) { // 依據當時的狀態決定不同的處理
-                    case FirstNumberInput:
-                        operand1=theValue;
-                        operand2=theValue;
-                        switch(bstr) {
+                switch(bstr) {
                             case "+": op=OP.Add; break;
                             case "-": op=OP.Sub; break;
                             case "*": op=OP.Mul; break;
                             case "/": op=OP.Div; break;
-                        }
-                        state=State.OperatorInputed;
-                        break;
-                    case OperatorInputed:
-                        switch(bstr) {
-                            case "+": op=OP.Add; break;
-                            case "-": op=OP.Sub; break;
-                            case "*": op=OP.Mul; break;
-                            case "/": op=OP.Div; break;
-                        }
-                        operand2=theValue;
-                        break;
-                    case NumberInput:
-                        operand2=theValue;
-                        switch(op) {
-                            case Add: theValue=operand1+operand2; break;
-                            case Sub: theValue=operand1-operand2; break;
-                            case Mul: theValue=operand1*operand2; break;
-                            case Div: theValue=operand1/operand2; break;
-                        }
-                        operand1=theValue;
-                        switch(bstr) {
-                            case "+": op=OP.Add; break;
-                            case "-": op=OP.Sub; break;
-                            case "*": op=OP.Mul; break;
-                            case "/": op=OP.Div; break;
-                        }
-                        state=State.OperatorInputed;
-                        edt.setText("" + theValue);
-                        break;
                 }
-                break;
+
+                    tempinput2=tempinput;
+                    tempinput="";
+                    edt.setText("" + tempinput);
+
+
+
+
+
+
+
             case "=": // 當＝號被點選時，依據當時的狀態決定不同的處理
-                if(state==State.OperatorInputed) {
+                if(isInteger(tempinput)&&isInteger(tempinput2))
+                {
+                    operand1=Integer.parseInt(tempinput2);
+                    operand2=Integer.parseInt(tempinput);
                     switch(op) {
                         case Add: theValue=operand1+operand2; break;
                         case Sub: theValue=operand1-operand2; break;
                         case Mul: theValue=operand1*operand2; break;
                         case Div: theValue=operand1/operand2; break;
+
                     }
-                    operand1=theValue;
-                }
-                else if(state==State.NumberInput) {
-                    operand2=theValue;
+                    edt.setText("" + theValue);
+                    tempinput="" +theValue;
+                } else if (isEmpty(tempinput)&&isEmpty(tempinput2)) {
+                    doperand1=Double.parseDouble(tempinput2);
+                    doperand2=Double.parseDouble(tempinput);
                     switch(op) {
-                        case Add: theValue=operand1+operand2; break;
-                        case Sub: theValue=operand1-operand2; break;
-                        case Mul: theValue=operand1*operand2; break;
-                        case Div: theValue=operand1/operand2; break;
+                        case Add: thedValue=doperand1+doperand2; break;
+                        case Sub: thedValue=doperand1-doperand2; break;
+                        case Mul: thedValue=doperand1*doperand2; break;
+                        case Div: thedValue=doperand1/doperand2; break;
+
                     }
-                    operand1=theValue;
-                    state=State.OperatorInputed;
+                    edt.setText("" + thedValue);
+                    tempinput="" +thedValue;
                 }
-                edt.setText("" + theValue);
+
+
+
                 break;
+        }
+    }
+    public boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public boolean isEmpty(String str) {
+        // 如果字符串的长度为0，则返回 true；否则返回 false
+        if (str.length() == 0)
+        {
+            return false;
+        }
+        else
+        {
+
+            return true ;
         }
     }
 }
