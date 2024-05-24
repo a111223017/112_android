4aac61808ba24ce963e7150f9140739c71521ca4
a111223017 <a111223017@mail.shu.edu.tw>
2024-05-24 15:00:13 +0800

    113-05-24

--- 0524/ExplicitlntentDemo2/app/src/main/java/com/example/explicitlntentdemo2/OpActivity.java
+++ 0524/ExplicitlntentDemo2/app/src/main/java/com/example/explicitlntentdemo2/OpActivity.java
@@ -2,7 +2,12 @@ package com.example.explicitlntentdemo2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 
 public class OpActivity extends AppCompatActivity {
 
@@ -10,5 +15,65 @@ public class OpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_op);
+        CheckBox chkDivide = (CheckBox)findViewById(R.id.chkDivide) ;
+        chkDivide.setEnabled(false);
+        Button button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int opd1,opd2;
+                double result = 0.0;
+                RadioButton rdbAdd,rdbSubtract,rdbMultiply,rdbDivide;
+
+                Bundle  bundle = OpActivity.this.getIntent().getExtras();
+                if(bundle == null)return;
+                opd1= Integer.parseInt(bundle.getString("OPERAND01"));
+                opd2= Integer.parseInt(bundle.getString("OPERAND02"));
+                rdbAdd=(RadioButton) findViewById(R.id.rdbAdd);
+                if (rdbAdd.isChecked())
+                {
+                    chkDivide.setEnabled(false);
+                    result = opd1+opd2;
+                }
+                rdbSubtract=(RadioButton) findViewById(R.id.rdbSubtract);
+                if (rdbSubtract.isChecked())
+                {
+                    chkDivide.setEnabled(false);
+                    result = opd1-opd2;
+                }
+                rdbMultiply=(RadioButton) findViewById(R.id.rdbMultiply);
+                if (rdbMultiply.isChecked())
+                {
+                    chkDivide.setEnabled(false);
+                    result = opd1*opd2;
+                }
+                rdbDivide=(RadioButton) findViewById(R.id.rdbDivide);
+
+
+                if (rdbDivide.isChecked())
+                {
+                    chkDivide.setEnabled(true);
+                    if(chkDivide.isChecked())
+                    {
+                        result = opd1 / opd2 ;
+                    }
+                    else
+                    {
+                        result = opd1 / (double) opd2;
+
+                    }
+                }
+                Intent rIntent = new Intent();
+                Bundle rBundle = new Bundle();
+
+                rBundle.putDouble("RESULT",result);
+                rIntent.putExtras(rBundle);
+                setResult(RESULT_OK,rIntent);
+                finish();
+
+
+            }
+        });
     }
 }
